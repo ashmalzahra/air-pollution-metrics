20ed531119a416515c1a7441e53ad3dd773fc5cc
ashmalzahra <ashmal.zahraa@gmail.com>
2023-03-18 14:16:57 +0500

    Final touches

--- src/components/Stocks.js
+++ src/components/Stocks.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchStocks } from '../redux/Stocks/stocksSlice';
 import { NavLink} from 'react-router-dom';
@@ -6,11 +6,19 @@ import './Stocks.css'
 import { AiOutlineStock } from "react-icons/ai";
 import { AiOutlineRightCircle } from 'react-icons/ai';
 import { AiOutlineBarChart } from 'react-icons/ai';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
+import { Form } from 'react-bootstrap';
 
 const Stocks = () => {
+    const [search, setSearch] = useState('');
     const dispatch = useDispatch()
     const stocksArray = useSelector((store) => store.stocks.stocks)
     const ifSucceed = useSelector((store) => store.stocks.ifSucceed)
+    const sum = stocksArray.reduce((n, { marketCap }) => n + marketCap, 0);
+
+    const handleChange = (e) => {
+        setSearch(e.target.value);
+      };
 
     useEffect(() => {
         if(stocksArray.length === 0){
@@ -18,25 +26,37 @@ const Stocks = () => {
         }
     }, [dispatch, ifSucceed, stocksArray])
 
-    return(
-        <main>
-                <section className='main-section'>
-                    <div className='top-icon'>
-                        <AiOutlineBarChart />
-                    </div>
-                    <div className='title'>
-                        <div>
-                            <h1 className='text'>TOTAL</h1>
-                            <h1 className='value'>$23847</h1>
-                        </div>
-                    </div>
-                </section>
-                <section className='heading'>
-                    STATS BY COMPANY
-                </section>
-                <section className='stocks'>
-                    {stocksArray.map((stock) => 
-                    <div className="stock">
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const Form = document.getElementById('searchForm');
+        Form.classList.replace('show', 'hide');
+        Form.reset();
+      };
+
+      const handleCloseSearch = () => {
+        const FormContainer = document.getElementById('searchFormContainer');
+        FormContainer.classList.replace('show', 'hide');
+        const Form = document.getElementById('searchForm');
+        Form.reset();
+        setSearch('');
+      };
+
+      const filteredArray = stocksArray.filter((stock) => {
+        if (search === '') { return stock; }
+        if (stock.symbol.toLowerCase().includes(search.toLowerCase())) {
+          return stock;
+        }
+        return '';
+      });
+
+      let stocksList = '';
+
+      if (filteredArray.length === 0) {
+        stocksList = 'NO COMPANIES FOUND';
+      } else {
+        stocksList = (
+          filteredArray.map((stock) => (
+            <div className="stock">
                     <NavLink to={`stock/${stock.symbol}`} className="symbol">
                     <div className="topHalf">
                         <div className="icon">
@@ -54,9 +74,45 @@ const Stocks = () => {
                     </div>
                     </NavLink>
                     </div>
-                    )}       
+                       ))
+                     );
+                   }
+
+    return(
+        <>
+        <main>
+                <section className='main-section'>
+                    <div className='top-icon'>
+                        <AiOutlineBarChart />
+                    </div>
+                    <div className='title'>
+                        <div>
+                            <span className='text'>TOTAL</span>
+                            <br />
+                            <span className='value'>${sum}</span>
+                        </div>
+                    </div>
+                </section>
+                <section className='heading'>
+                    STATS BY COMPANY
+                </section>
+                <section className='stocks'>
+                {stocksList}
                 </section>
         </main>
+        <section id="searchFormContainer" className="hide">
+        <Form id="searchForm" onSubmit={handleSubmit}>
+          <Form.Control
+            type="text"
+            placeholder="Search company ticker"
+            onChange={(e) => handleChange(e)}
+          />
+          <div id="closeSearch">
+            <AiOutlineCloseCircle onClick={handleCloseSearch} />
+          </div>
+        </Form>
+      </section>
+      </>
     )
 }
 
